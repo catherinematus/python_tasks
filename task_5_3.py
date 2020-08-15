598592917077eaa6ee79c69c2807a22a43e8e412
catherinematus <matus34ka@gmail.com>
2020-08-16 00:35:48 +0300

    task_5_3

--- task_5_3.py
+++ task_5_3.py
@@ -1,12 +1,12 @@
-for n in range(200, 300):
+for d in range(200, 300):
     sum = 0
-    for i in range(1, n):
-        if n % i == 0:
+    for i in range(1, d):
+        if d % i == 0:
             sum += i
-    if sum > n:
+    if sum > d:
         result = 0
         for j in range(1, sum):
             if sum % j == 0:
                 result += j
-        if result == n:
-            print(n, sum)
+        if result == d:
+            print(d, sum)
